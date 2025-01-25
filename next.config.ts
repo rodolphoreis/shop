d3c2604a4fe2777feb38a5fc8f014b00fa48edae
Rodolpho Reis <rodolphoreis@live.com>
2025-01-25 13:40:33 +0000

    feat: add 'cdn.vnda.com.br' to Next.js image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["cdn.vnda.com.br"],
+  },
 };
 
 export default nextConfig;
