580fd22878c4ef156cffe0def0bf4e6286fda6d7
Rodolpho Reis <rodolphoreis@live.com>
2025-01-25 13:43:11 +0000

    feat: add Header component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Shop",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased p-5`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
