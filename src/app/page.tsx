af212c48cb5301a37a320996269b80cd48bc9248
Rodolpho Reis <rodolphoreis@live.com>
2025-01-27 18:58:03 +0000

    feat: add CardProduct and CarouselPlugin components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
-import { CarouselSpacing } from "@/components/carouselSpacing";
-import Image from "next/image";
-
+import CardProduct from "@/components/card-product";
+import { CarouselPlugin } from "@/components/CarouselPlugin";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="w-full h-screen">
-      <div className="w-full max-w-[1200px] mx-auto bg-red-50">
-        <Image
-          src="/sale.png"
-          alt="sale"
-          width={1200}
-          height={400}
-          className="w-full max-w-[1200px] h-auto rounded-md"
-          style={{ maxHeight: "350px" }}
-        />
+      <div className="flex mx-auto w-screen ">
+        <CarouselPlugin />
       </div>
-      <div className="flex mx-auto max-w-[1160px] px-10 sm:p-2">
-        <CarouselSpacing />
+      <div className="w-full mx-auto">
+        <CardProduct />
       </div>
     </div>
   );
