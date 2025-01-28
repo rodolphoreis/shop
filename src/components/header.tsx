781250d992eabeced8fb43dd420fcde02bd07ade
Rodolpho Reis <rodolphoreis@live.com>
2025-01-28 14:18:13 +0000

    style: change h-9 for h-10

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
           <Filter />
         </div>
       </div>
-      <div className="w-full  h-9 bg-zinc-100 flex mx-auto items-center justify-center">
+      <div className="w-full  h-10 bg-zinc-100 flex mx-auto items-center justify-center">
         <MenuBar />
       </div>
     </div>
