d4ead500336ab3ca5249dc3ca5ca85431ab02b84
Rodolpho Reis <rodolphoreis@live.com>
2025-01-28 14:23:50 +0000

    feat: add Image in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import CardProduct from "@/components/card-product";
-import { CarouselPlugin } from "@/components/CarouselPlugin";
+import Image from "next/image";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="w-full h-screen">
-      <div className="flex mx-auto w-screen ">
-        <CarouselPlugin />
+      <div className="flex mx-auto w-full m-0 p-0 mt-16">
+        <Image
+          src={
+            "https://cdn.vnda.com.br/lolja/2025/01/27/11_1_8_839_FULLHOMEDimensosugerida2280x1280pixels.jpg?v=1737986786"
+          }
+          alt="banner"
+          className="object-cover w-full max-h-[600px] m-0 p-0"
+          width={3420}
+          height={680}
+        />
       </div>
       <div className="w-full mx-auto">
         <CardProduct />
