fb245128fcf2dd3854f364c5ddd90db39b871bb0
Rodolpho Reis <rodolphoreis@live.com>
2025-01-29 18:18:04 +0000

    feat: add search input with icon for filtering products

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Input } from "./ui/input";
+import { Search } from "lucide-react";
 
-const Filter = () => {
+const Filter = ({ onFilter }: { onFilter: (searchTerm: string) => void }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleFilter(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    setSearchTerm(value);
+    onFilter(value);
+  }
+
   return (
-    <div className="sm:w-96">
+    <div className="flex items-center  sm:w-96 border border-zinc-200 rounded-md p-1">
       <Input
-        type="email"
+        type="text"
         placeholder="Busque por produtos.."
-        className="flex-1 text-sm"
+        className="flex-1 text-sm border-none shadow-none "
+        value={searchTerm}
+        onChange={handleFilter}
       />
+      <Search className="text-zinc-300 mr-2" size={20} />
     </div>
   );
 };
