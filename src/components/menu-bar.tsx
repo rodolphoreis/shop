e5a7333f2f00cf3e34809d64f6c777d7cb56542b
Rodolpho Reis <rodolphoreis@live.com>
2025-01-29 17:38:54 +0000

    Update Menubar items with correct names and filter function.

--- src/components/menu-bar.tsx
+++ src/components/menu-bar.tsx
@@ -3,160 +3,59 @@ import React from "react";
 
 import {
   Menubar,
-  MenubarCheckboxItem,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarRadioGroup,
-  MenubarRadioItem,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarSub,
-  MenubarSubContent,
-  MenubarSubTrigger,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 
-const MenuBar = () => {
+const MenuBar = ({ onFilter }: { onFilter: (produto: string) => void }) => {
   return (
     <div className="w-full h-10 bg-zinc-100 flex mx-auto items-center justify-center">
       <Menubar className="flex justify-center bg-zinc-100 text-zinc-500 mx-auto text-xs sm:text-sm border-none shadow-none w-full max-w-screen-lg md:gap-24 items-center overflow-hidden">
         <MenubarMenu>
-          <MenubarTrigger className="truncate px-2">COLEÇÕES</MenubarTrigger>
+          <MenubarTrigger className="truncate px-2 hover:text-zinc-700">
+            COLEÇÕES
+          </MenubarTrigger>
           <MenubarContent>
-            <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            <MenubarItem onClick={() => onFilter("Todos")}>Todos</MenubarItem>
+
+            <MenubarItem onClick={() => onFilter("Camiseta")}>
+              Camisetas
             </MenubarItem>
-            <MenubarItem>
-              New Window <MenubarShortcut>⌘N</MenubarShortcut>
+            <MenubarItem onClick={() => onFilter("Moletom")}>
+              Moletom
             </MenubarItem>
-            <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-              New Incognito Window
+            <MenubarItem onClick={() => onFilter("Boné")}>Boné</MenubarItem>
+            <MenubarItem onClick={() => onFilter("Caneca")}>Caneca</MenubarItem>
+            <MenubarItem onClick={() => onFilter("Almofada")}>
+              Almofada
             </MenubarItem>
-            <MenubarSeparator />
-            <MenubarSub>
-              <MenubarSubTrigger className="truncate px-2">
-                CAMISETAS
-              </MenubarSubTrigger>
-              <MenubarSubContent>
-                <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-                  Email link
-                </MenubarItem>
-                <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-                  Messages
-                </MenubarItem>
-                <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-                  Notes
-                </MenubarItem>
-              </MenubarSubContent>
-            </MenubarSub>
-            <MenubarSeparator />
-            <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-              Print... <MenubarShortcut>⌘P</MenubarShortcut>
-            </MenubarItem>
+            <MenubarItem onClick={() => onFilter("Ecobag")}>Ecobag</MenubarItem>
           </MenubarContent>
         </MenubarMenu>
         <MenubarMenu>
           <MenubarTrigger className="truncate px-2">MOLETON</MenubarTrigger>
           <MenubarContent>
-            <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-              Undo <MenubarShortcut>⌘Z</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-              Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarSub>
-              <MenubarSubTrigger className="truncate px-2">
-                PARCEIROS
-              </MenubarSubTrigger>
-              <MenubarSubContent>
-                <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-                  Search the web
-                </MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-                  Find...
-                </MenubarItem>
-                <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-                  Find Next
-                </MenubarItem>
-                <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-                  Find Previous
-                </MenubarItem>
-              </MenubarSubContent>
-            </MenubarSub>
-            <MenubarSeparator />
-            <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-              Cut
-            </MenubarItem>
-            <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-              Copy
-            </MenubarItem>
-            <MenubarItem className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate">
-              Paste
+            <MenubarItem onClick={() => onFilter("Todos")}>Todos</MenubarItem>
+            <MenubarItem onClick={() => onFilter("Moletom")}>
+              Moletom
             </MenubarItem>
           </MenubarContent>
         </MenubarMenu>
         <MenubarMenu>
           <MenubarTrigger className="truncate px-2">ACESSÓRIOS</MenubarTrigger>
           <MenubarContent>
-            <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
-            <MenubarCheckboxItem checked>
-              Always Show Full URLs
-            </MenubarCheckboxItem>
-            <MenubarSeparator />
-            <MenubarItem
-              inset
-              className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate"
-            >
-              Reload <MenubarShortcut>⌘R</MenubarShortcut>
+            <MenubarItem onClick={() => onFilter("Todos")}>Todos</MenubarItem>
+            <MenubarItem onClick={() => onFilter("Caneca")}>Caneca</MenubarItem>
+            <MenubarItem onClick={() => onFilter("Boné")}>Boné</MenubarItem>
+            <MenubarItem onClick={() => onFilter("Almofada")}>
+              Almofada
             </MenubarItem>
-            <MenubarItem
-              disabled
-              inset
-              className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate"
-            >
-              Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+            <MenubarItem onClick={() => onFilter("Chaveiro")}>
+              Chaveiro
             </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem
-              inset
-              className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate"
-            >
-              Toggle Fullscreen
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem
-              inset
-              className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate"
-            >
-              Hide Sidebar
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger className="truncate px-2">ARTISTAS</MenubarTrigger>
-          <MenubarContent>
-            <MenubarRadioGroup value="benoit">
-              <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
-              <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
-              <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
-            </MenubarRadioGroup>
-            <MenubarSeparator />
-            <MenubarItem
-              inset
-              className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate"
-            >
-              Edit...
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem
-              inset
-              className="w-full sm:w-auto px-2 py-1 text-xs md:text-sm truncate"
-            >
-              Add Profile...
-            </MenubarItem>
+            <MenubarItem onClick={() => onFilter("Ecobag")}>Ecobag</MenubarItem>
           </MenubarContent>
         </MenubarMenu>
       </Menubar>
