8e6a17052f79c1c3acbff9e67af65f21de8cd2a5
Rodolpho Reis <rodolphoreis@live.com>
2025-01-29 17:36:33 +0000

    feat: Add product filtering functionality to Home component. Integrate filtering with MenuBar and CardProduct.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,57 @@
+"use client";
+import { useState } from "react";
 import CardProduct from "@/components/card-product";
+import produtos from "@/data/products";
 import Image from "next/image";
 import React from "react";
+import MenuBar from "@/components/menu-bar";
 
 const Home = () => {
+  const [filteredProducts, setFilteredProducts] = useState(produtos);
+
+  function handleFilter(categoria: string) {
+    if (categoria === "Todos") {
+      setFilteredProducts(produtos);
+    } else if (categoria === "Moletom") {
+      setFilteredProducts(
+        produtos.filter((item) => item.produto === "Moletom")
+      );
+    } else if (categoria === "Acessórios") {
+      const acessorios = ["Caneca", "Boné", "Almofada", "Chaveiro", "Ecobag"];
+      setFilteredProducts(
+        produtos.filter((item) => acessorios.includes(item.produto))
+      );
+    } else {
+      setFilteredProducts(
+        produtos.filter((item) => item.produto === categoria)
+      );
+    }
+  }
+
   return (
-    <div className="w-full h-screen">
-      <div className="flex mx-auto w-full m-0 p-0 mt-16">
-        <Image
-          src={
-            "https://cdn.vnda.com.br/lolja/2025/01/27/11_1_8_839_FULLHOMEDimensosugerida2280x1280pixels.jpg?v=1737986786"
-          }
-          alt="banner"
-          className="object-cover w-full max-h-[600px] m-0 p-0"
-          width={3420}
-          height={680}
-        />
+    <>
+      <div className="w-full mt-7 h-10 bg-zinc-100 flex mx-auto items-center justify-center px-1">
+        <MenuBar onFilter={handleFilter} />
       </div>
-      <div className="w-full mx-auto">
-        <CardProduct />
+
+      <div className="w-full h-screen">
+        <div className="flex mx-auto w-full m-0 p-0 mt-0">
+          <Image
+            src={
+              "https://cdn.vnda.com.br/lolja/2025/01/27/11_1_8_839_FULLHOMEDimensosugerida2280x1280pixels.jpg?v=1737986786"
+            }
+            alt="banner"
+            className="object-cover w-full max-h-[600px] m-0 p-0"
+            width={3420}
+            height={680}
+          />
+        </div>
+
+        <div className="w-full mx-auto">
+          <CardProduct produtos={filteredProducts} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
