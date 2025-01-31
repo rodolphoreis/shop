7e435774663e43ccd19e86cb32959b7095ce6d8f
Rodolpho Reis <rodolphoreis@live.com>
2025-01-31 12:38:02 +0000

    style: change color

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -92,7 +92,7 @@ const ProductId = ({ params }: PageProps) => {
           <Link
             href="/"
             className={cn(
-              "w-full sm:w-[60%] justify-center text-center px-6 py-2 bg-zinc-400 hover:bg-orange-400 text-white rounded-lg shadow-md transition duration-300"
+              "w-full sm:w-[60%] justify-center text-center px-6 py-2 bg-zinc-400 hover:bg-orange-300 text-white rounded-lg shadow-md transition duration-300"
             )}
           >
             Voltar a Home
