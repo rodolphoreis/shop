afcd1a352e3b06ed193b5ce0f392fe4571bd4d55
Rodolpho Reis <rodolphoreis@live.com>
2025-01-31 12:15:03 +0000

    feat: add navigation to product details page with button click

--- src/components/card-product.tsx
+++ src/components/card-product.tsx
@@ -4,8 +4,13 @@ import React from "react";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardFooter } from "./ui/card";
 import { ProductsInterface } from "@/data/products";
+import { useRouter } from "next/navigation";
 
 const CardProduct = ({ produtos }: { produtos: ProductsInterface[] }) => {
+  const navigate = useRouter();
+  function handleProduct(id: number) {
+    navigate.push(`/product/${id}`);
+  }
   return (
     <div className="flex w-full justify-center  sm:mt-0 py-10">
       <div className="grid grid-cols-2 gap-5 md:gap-10  mt-5 sm:grid-cols-2 lg:grid-cols-5 justify-center mx-auto w-[90%] px-4 ">
@@ -27,14 +32,19 @@ const CardProduct = ({ produtos }: { produtos: ProductsInterface[] }) => {
               <h2 className="text-xs sm:text-sm font-semibold">
                 {produto.produto}
               </h2>
-              <p className="text-xs sm:text-sm text-gray-500">
-                € {produto.preco}
-              </p>
+              <div className="flex gap-2 items-center">
+                <p className="text-red-500 line-through text-xs sm:text-sm">
+                  € {produto.preco}
+                </p>
+              </div>
             </CardContent>
 
             <div className="relative ">
-              <CardFooter className="absolute bg-zinc-400/30  inset-x-0 bottom-0 transform translate-y-full transition-transform duration-300 ease-in-out group-hover:translate-y-3 items-center pt-3">
-                <Button className=" flex items-center w-full transition-all duration-300 hover:text-zinc-100 bg-orange-500 hover:bg-orange-300 mt-1">
+              <CardFooter className="absolute bg-zinc-400/30  inset-x-0 bottom-0 transform translate-y-full transition-transform duration-300 ease-in-out group-hover:translate-y-3 items-center pt-2 sm:pt-3">
+                <Button
+                  onClick={() => handleProduct(produto.id)}
+                  className=" flex items-center w-full transition-all duration-300 hover:text-zinc-100 bg-orange-500 hover:bg-orange-300 mt-1"
+                >
                   Comprar
                 </Button>
               </CardFooter>
